8d6bdc722cb3c48fdb0583c41ffed8c0a358bcbf
Subarna1997 <86381545+Subarna1997@users.noreply.github.com>
2021-08-30 12:27:37 +0530

    Add files via upload

---
+++ Employee/src/main/java/com/example/Employee/ApiStatusCode.java
@@ -0,0 +1,9 @@
+package com.example.Employee;
+
+public class ApiStatusCode {
+	public static final int EMPLOYEE_NOT_FOUND = 900;
+	public static final int NULL_VALUE = 650;
+	public static final int USER_NOT_FOUND = 750;
+	
+
+}
